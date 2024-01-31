7fa6acaaeef665af9b90f140488d8c8b31ea32b8
Dani Garcia <dgarcia@irontec.com>
2024-01-31 10:47:12 +0100

    bin: add ivoz:make:repositories to generate-entitites.sh

--- app/bin/generate-entities.sh
+++ app/bin/generate-entities.sh
@@ -5,4 +5,5 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 pushd ${SCRIPT_DIR}/../
   bin/console ivoz:make:entities Demo
   bin/console ivoz:make:interfaces Demo
+  bin/console ivoz:make:repositories Demo
 popd
