e31c5dc05a6ffb717b45809aa4d5e3e0da7f0eff
Dani Garcia <dgarcia@irontec.com>
2024-01-31 15:41:07 +0100

    demo/model: add Timezone repository

---
+++ app/src/Demo/Domain/Model/Timezone/TimezoneRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Demo\Domain\Model\Timezone;
+
+use Ivoz\Core\Domain\Service\Repository\RepositoryInterface;
+
+/**
+ * @extends RepositoryInterface<TimezoneInterface, TimezoneDto>
+ */
+interface Timezone extends RepositoryInterface
+{
+}
