f49af5f301124b32fb42b70eae970107564718d8
Dani Garcia <dgarcia@irontec.com>
2024-02-01 17:08:09 +0100

    amend! domain/model: add Timezone table and entity
    domain/model: add Timezone table and entity

--- app/migrations/Version20240201122140.php
+++ app/migrations/Version20240201122140.php
@@ -14,7 +14,7 @@ final class Version20240201122140 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add Timezone table';
     }
 
     public function up(Schema $schema): void
